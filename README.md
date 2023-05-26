928e3c2fb60e90137ec21c2ae584815e5cff0664
Jarod Umland <jarodumland@gmail.com>
2023-05-26 14:16:51 +0200

    styling and placeholder

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # Studienprojekt1_Robotersteuerung
 
-# To start the Gui navigate to /webserver
-# then type:
-
-    python -m flask --app GUI run
-
-# to serve it in your browser
--- Webserver/GUI.py
+++ Webserver/GUI.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, url_for
+import requests
 
 app = Flask("Robot Controls")
 
@@ -11,6 +12,37 @@ def index():
 def controls():
         return render_template('controls.html')
 
+@app.route("/forward/", methods=['POST'])
+def move_forward():
+    #Moving forward code
+    forward_message = "Moving Forward..."
+        #Björn Funktionen
+    return render_template('index.html', forward_message=forward_message);
+
+@app.route("/backward/", methods=['POST'])
+def move_backward():
+    #Moving forward code
+    backward_message = "Moving Back..."
+    #Björn Funktionen
+    return render_template('index.html', forward_message=backward_message);
+
+@app.route("/leftturn/", methods=['POST'])
+def turn_left():
+    #Turn left code
+    left_message = "Turning Left..."
+        #Björn Funktionen
+
+    return render_template('index.html', forward_message=left_message);
+
+@app.route("/rightturn/", methods=['POST'])
+def turn_right():
+    #Moving forward code
+    right_message = "Turning Right..."
+        #Björn Funktionen
+
+    return render_template('index.html', forward_message=right_message);
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
