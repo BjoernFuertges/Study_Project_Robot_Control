8a7f45a40acecbdceaf572c95a2d037ad09c8bb9
Bjoern Fuertges <bjoern.fuertges@bayer.com>
2023-05-30 13:09:12 +0200

    Additional change methods

--- robotcontroll/webcontroller/webcontroller.py
+++ robotcontroll/webcontroller/webcontroller.py
@@ -13,23 +13,76 @@ class Agent(webcontroller_pb2_grpc.AgentServicer):
     sm = status_management.status_manager()
 
     def MoveInformation(self, request, context):
-        return webcontroller_pb2.MoveInformationReply(name='Hello, %s!' % request.name, stop=False, speed=100, direction="forward", turn="no", radius=0.8)
+        return self.Sm_To_mir()
     
+    def MoveInformationGetLastSended(self, request, context):
+        return self.Sm_To_mirws()
+
     def MoveInformationDeliveryLeft (self, request, context):
-        return webcontroller_pb2.MoveInformationReply(name='Hello, %s!' % request.name, stop=False, speed=100, direction="forward", turn="no", radius=0.8)
+        self.sm.set_turn_speed_radius('left', request.speed, request.radius)
+        return self.Sm_To_mir()
     
     def MoveInformationDeliveryRight (self, request, context):
-        return webcontroller_pb2.MoveInformationReply(name='Hello, %s!' % request.name, stop=False, speed=100, direction="forward", turn="no", radius=0.8)
+        self.sm.set_turn_speed_radius('right', request.speed, request.radius)
+        return self.Sm_To_mir()
     
+    def MoveInformationDeliveryCenter (self, request, context):
+        self.sm.set_turn_speed_radius('no', request.speed, request.radius)
+        return self.Sm_To_mir()
+
     def MoveInformationDeliveryForward (self, request, context):
-        return webcontroller_pb2.MoveInformationReply(name='Hello, %s!' % request.name, stop=False, speed=100, direction="forward", turn="no", radius=0.8)
+        self.sm.set_direction_speed_radius('forward', request.speed, request.radius)
+        return self.Sm_To_mir()
     
     def MoveInformationDeliveryBackward (self, request, context):
-        return webcontroller_pb2.MoveInformationReply(name='Hello, %s!' % request.name, stop=False, speed=100, direction="forward", turn="no", radius=0.8)
+        self.sm.set_direction_speed_radius('backward', request.speed, request.radius)
+        return self.Sm_To_mir()
+    
+    def MoveInformationDeliveryChangeLeft (self, request, context):
+        self.sm.turn = 'left'
+        return self.Sm_To_mir()
+    
+    def MoveInformationDeliveryChangeRight (self, request, context):
+        self.sm.turn = 'right'
+        return self.Sm_To_mir()
+    
+    def MoveInformationDeliveryChangeCenter (self, request, context):
+        self.sm.turn = 'no'
+        return self.Sm_To_mir()
+
+    def MoveInformationDeliveryChangeForward (self, request, context):
+        self.sm.direction = 'forward'
+        return self.Sm_To_mir()
+    
+    def MoveInformationDeliveryChangeBackward (self, request, context):
+        self.sm.direction = 'backward'
+        return self.Sm_To_mir()
     
     def MoveInformationDeliveryStop (self, request, context):
-        return webcontroller_pb2.MoveInformationReply(name='Hello, %s!' % request.name, stop=False, speed=100, direction="forward", turn="no", radius=0.8)
+        self.sm.stop = request.stop
+        return self.Sm_To_mir()
     
+    def Sm_To_mir(self) -> webcontroller_pb2.MoveInformationReply:
+        return webcontroller_pb2.MoveInformationReply(
+            self.sm.name, 
+            self.sm.stop,
+            self.sm.speed,
+            self.sm.direction,
+            self.sm.turn,
+            self.sm.radius
+        )
+    
+    def Sm_To_mirws(self) -> webcontroller_pb2.MoveInformationReplyWithStatus:
+        return webcontroller_pb2.MoveInformationReplyWithStatus(
+            self.sm.name, 
+            self.sm.stop,
+            self.sm.speed,
+            self.sm.direction,
+            self.sm.turn,
+            self.sm.radius,
+            self.sm.passed_to_robot
+        )
+
 def serve():
     port = '50051'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
