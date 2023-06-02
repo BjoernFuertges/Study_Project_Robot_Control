76b2506ed2f19d244d569aab541a495078ad8d28
Bjoern Fuertges <github.bjoern@fuertges.eu>
2023-06-02 11:03:58 +0200

    rcs: added argparse in handler

--- robotcontrol/robot_control_system/leader.py
+++ robotcontrol/robot_control_system/leader.py
@@ -1,18 +1,16 @@
 from queue import Queue
 from threading import Thread
-import move
-from move import Move_Command
+import robot_control_system.move
+from robot_control_system.move import Move_Command
 
 import grpc
 from protos_generated import webcontroller_pb2
 from protos_generated import webcontroller_pb2_grpc
 
-robot_name = "IRobot"
-
 # A thread that produces data
-def ui(out_q):
+def ui(out_q, robot_name : str, wc_ip : str, wc_port : int):
     while(True):
-        with grpc.insecure_channel('localhost:50051') as channel:
+        with grpc.insecure_channel(wc_ip + ":" + str(wc_port)) as channel:
             stub = webcontroller_pb2_grpc.AgentStub(channel)
             response = stub.MoveInformationHasNew(webcontroller_pb2.MoveInformationRequest(name=robot_name))
             
@@ -31,10 +29,10 @@ def ui(out_q):
                 if mc.get_stop_working:
                     break
           
-def start() -> None:
+def start(name : str, wc_ip : str, wc_port : int) -> None:
     # Create the shared queue and launch both threads
     working_queue = Queue()
-    t_mh = Thread(target = move.move_handler, args =(working_queue, ))
+    t_mh = Thread(target = move.move_handler, args =(working_queue, name, wc_ip, wc_port,))
     t_ui = Thread(target = ui, args =(working_queue, ))
     t_mh.start()
     t_ui.start()
