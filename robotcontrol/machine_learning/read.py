0b24ada45cfd4f1d8012a0906e8a53ce07f43932
Jarod Umland <jarodumland@gmail.com>
2023-06-16 14:17:25 +0200

    changed readme

---
+++ robotcontrol/machine_learning/read.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+#reads the image then returns it
+img = cv.imread('Photos/test.jpg')
+
+#opens the image in a new window
+cv.imshow('test', img)
+#large images can go off screen
+
+#reading videos
+
+capture = cv.VideoCapture('Videos/test.mov')
+#reading video frame by frame
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+#capture.release()
+#cv.destroyAllWindows
+
+
+
+#wait till cv.waitKey(3)
