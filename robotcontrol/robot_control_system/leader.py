0923541a52b1aaebd6d0f6ed400b7840fe79f997
Bjoern Fuertges <github.bjoern@fuertges.eu>
2023-06-17 21:26:17 +0200

    rcs: Move is now a class

--- robotcontrol/robot_control_system/leader.py
+++ robotcontrol/robot_control_system/leader.py
@@ -38,7 +38,8 @@ def ui(out_q, robot_name : str, wc_ip : str, wc_port : int):
 def start(name : str, wc_ip : str, wc_port : int) -> None:
     # Create the shared queue and launch both threads
     working_queue = Queue()
-    t_mh = Thread(target = move.move_handler, args =(working_queue, ))
+    m = move.Move()
+    t_mh = Thread(target = m.move_handler, args =(working_queue, ))
     t_ui = Thread(target = ui, args =(working_queue, name, wc_ip, wc_port,))
     t_mh.start()
     t_ui.start()
--- robotcontrol/robot_control_system/move.py
+++ robotcontrol/robot_control_system/move.py
@@ -8,168 +8,167 @@
 import time
 import RPi.GPIO as GPIO
 import robot_control_system.RGB as RGB
+from robot_control_system.move_command import Move_Command
 
-# motor_EN_A: Pin7  |  motor_EN_B: Pin11
-# motor_A:  Pin8,Pin10    |  motor_B: Pin13,Pin12
+class Move:
 
-Motor_A_EN    = 4
-Motor_B_EN    = 17
+	# motor_EN_A: Pin7  |  motor_EN_B: Pin11
+	# motor_A:  Pin8,Pin10    |  motor_B: Pin13,Pin12
 
-Motor_A_Pin1  = 14
-Motor_A_Pin2  = 15
-Motor_B_Pin1  = 27
-Motor_B_Pin2  = 18
+	Motor_A_EN    = 4
+	Motor_B_EN    = 17
 
-Dir_forward   = 0
-Dir_backward  = 1
+	Motor_A_Pin1  = 14
+	Motor_A_Pin2  = 15
+	Motor_B_Pin1  = 27
+	Motor_B_Pin2  = 18
 
-left_forward  = 0
-left_backward = 1
+	Dir_forward   = 0
+	Dir_backward  = 1
 
-right_forward = 0
-right_backward= 1
+	left_forward  = 0
+	left_backward = 1
 
-pwn_A = 0
-pwm_B = 0
+	right_forward = 0
+	right_backward= 1
 
-setup_completed = False
+	pwn_A = 0
+	pwm_B = 0
 
-def motorStop() -> None:#Motor stops
-	GPIO.output(Motor_A_Pin1, GPIO.LOW)
-	GPIO.output(Motor_A_Pin2, GPIO.LOW)
-	GPIO.output(Motor_B_Pin1, GPIO.LOW)
-	GPIO.output(Motor_B_Pin2, GPIO.LOW)
-	GPIO.output(Motor_A_EN, GPIO.LOW)
-	GPIO.output(Motor_B_EN, GPIO.LOW)
+	rgb : RGB.RGB
 
+	def __init__(self):#Motor initialization
+		rgb = RGB.RGB()
+		
+		rgb.red()
+		setup()
 
-def setup() -> None:#Motor initialization
-	global pwm_A, pwm_B
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(Motor_A_EN, GPIO.OUT)
-	GPIO.setup(Motor_B_EN, GPIO.OUT)
-	GPIO.setup(Motor_A_Pin1, GPIO.OUT)
-	GPIO.setup(Motor_A_Pin2, GPIO.OUT)
-	GPIO.setup(Motor_B_Pin1, GPIO.OUT)
-	GPIO.setup(Motor_B_Pin2, GPIO.OUT)
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.Motor_A_EN, GPIO.OUT)
+		GPIO.setup(self.Motor_B_EN, GPIO.OUT)
+		GPIO.setup(self.Motor_A_Pin1, GPIO.OUT)
+		GPIO.setup(self.Motor_A_Pin2, GPIO.OUT)
+		GPIO.setup(self.Motor_B_Pin1, GPIO.OUT)
+		GPIO.setup(self.Motor_B_Pin2, GPIO.OUT)
 
-	motorStop()
-	try:
-		pwm_A = GPIO.PWM(Motor_A_EN, 1000)
-		pwm_B = GPIO.PWM(Motor_B_EN, 1000)
-	except:
-		pass
+		motorStop()
+		try:
+			pwm_A = GPIO.PWM(self.Motor_A_EN, 1000)
+			self.pwm_B = GPIO.PWM(self.Motor_B_EN, 1000)
+		except:
+			pass
 
+		rgb.blue()
 
-def motor_left(status : int, direction : int, speed : int) -> None:#Motor 2 positive and negative rotation
-	if status == 0: # stop
-		GPIO.output(Motor_B_Pin1, GPIO.LOW)
-		GPIO.output(Motor_B_Pin2, GPIO.LOW)
-		GPIO.output(Motor_B_EN, GPIO.LOW)
-	else:
-		if direction == Dir_backward:
-			GPIO.output(Motor_B_Pin1, GPIO.HIGH)
-			GPIO.output(Motor_B_Pin2, GPIO.LOW)
-			pwm_B.start(100)
-			pwm_B.ChangeDutyCycle(speed)
-		elif direction == Dir_forward:
-			GPIO.output(Motor_B_Pin1, GPIO.LOW)
-			GPIO.output(Motor_B_Pin2, GPIO.HIGH)
-			pwm_B.start(0)
-			pwm_B.ChangeDutyCycle(speed)
+	def __del__(self):
+		motorStop()
+		GPIO.cleanup()             # Release resource
+
+	def motorStop(self) -> None:#Motor stops
+		GPIO.output(self.Motor_A_Pin1, GPIO.LOW)
+		GPIO.output(self.Motor_A_Pin2, GPIO.LOW)
+		GPIO.output(self.Motor_B_Pin1, GPIO.LOW)
+		GPIO.output(self.Motor_B_Pin2, GPIO.LOW)
+		GPIO.output(self.Motor_A_EN, GPIO.LOW)
+		GPIO.output(self.Motor_B_EN, GPIO.LOW)
 
 
-def motor_right(status : int, direction : int, speed : int) -> None:#Motor 1 positive and negative rotation
-	if status == 0: # stop
-		GPIO.output(Motor_A_Pin1, GPIO.LOW)
-		GPIO.output(Motor_A_Pin2, GPIO.LOW)
-		GPIO.output(Motor_A_EN, GPIO.LOW)
-	else:
-		if direction == Dir_forward:#
-			GPIO.output(Motor_A_Pin1, GPIO.HIGH)
-			GPIO.output(Motor_A_Pin2, GPIO.LOW)
-			pwm_A.start(100)
-			pwm_A.ChangeDutyCycle(speed)
-		elif direction == Dir_backward:
-			GPIO.output(Motor_A_Pin1, GPIO.LOW)
-			GPIO.output(Motor_A_Pin2, GPIO.HIGH)
-			pwm_A.start(0)
-			pwm_A.ChangeDutyCycle(speed)
 
 
-def move(speed : int, direction : str, turn : str, radius : float = 0.6):   # 0 < radius <= 1  
-	#speed = 100
-	if direction == 'forward':
-		if turn == 'right':
-			motor_left(0, left_backward, int(speed*radius))
-			motor_right(1, right_forward, speed)
-		elif turn == 'left':
-			motor_left(1, left_forward, speed)
-			motor_right(0, right_backward, int(speed*radius))
+	def motor_left(self, status : int, direction : int, speed : int) -> None:#Motor 2 positive and negative rotation
+		if status == 0: # stop
+			GPIO.output(self.Motor_B_Pin1, GPIO.LOW)
+			GPIO.output(self.Motor_B_Pin2, GPIO.LOW)
+			GPIO.output(self.Motor_B_EN, GPIO.LOW)
 		else:
-			motor_left(1, left_forward, speed)
-			motor_right(1, right_forward, speed)
-	elif direction == 'backward':
-		if turn == 'right':
-			motor_left(0, left_forward, int(speed*radius))
-			motor_right(1, right_backward, speed)
-		elif turn == 'left':
-			motor_left(1, left_backward, speed)
-			motor_right(0, right_forward, int(speed*radius))
+			if direction == self.Dir_backward:
+				GPIO.output(self.Motor_B_Pin1, GPIO.HIGH)
+				GPIO.output(self.Motor_B_Pin2, GPIO.LOW)
+				self.pwm_B.start(100)
+				self.pwm_B.ChangeDutyCycle(speed)
+			elif direction == self.Dir_forward:
+				GPIO.output(self.Motor_B_Pin1, GPIO.LOW)
+				GPIO.output(self.self.Motor_B_Pin2, GPIO.HIGH)
+				self.pwm_B.start(0)
+				self.pwm_B.ChangeDutyCycle(speed)
+
+
+	def motor_right(self, status : int, direction : int, speed : int) -> None:#Motor 1 positive and negative rotation
+		if status == 0: # stop
+			GPIO.output(self.Motor_A_Pin1, GPIO.LOW)
+			GPIO.output(self.Motor_A_Pin2, GPIO.LOW)
+			GPIO.output(self.Motor_A_EN, GPIO.LOW)
 		else:
-			motor_left(1, left_backward, speed)
-			motor_right(1, right_backward, speed)
-	elif direction == 'no':
-		if turn == 'right':
-			motor_left(1, left_backward, speed)
-			motor_right(1, right_forward, speed)
-		elif turn == 'left':
-			motor_left(1, left_forward, speed)
-			motor_right(1, right_backward, speed)
+			if direction == self.Dir_forward:#
+				GPIO.output(self.Motor_A_Pin1, GPIO.HIGH)
+				GPIO.output(self.Motor_A_Pin2, GPIO.LOW)
+				pwm_A.start(100)
+				pwm_A.ChangeDutyCycle(speed)
+			elif direction == self.Dir_backward:
+				GPIO.output(self.Motor_A_Pin1, GPIO.LOW)
+				GPIO.output(self.self.Motor_A_Pin2, GPIO.HIGH)
+				pwm_A.start(0)
+				pwm_A.ChangeDutyCycle(speed)
+
+
+	def move(self, speed : int, direction : str, turn : str, radius : float = 0.6):   # 0 < radius <= 1  
+		#speed = 100
+		if direction == 'forward':
+			if turn == 'right':
+				motor_left(0, self.left_backward, int(speed*radius))
+				motor_right(1, self.right_forward, speed)
+			elif turn == 'left':
+				motor_left(1, self.left_forward, speed)
+				motor_right(0, self.right_backward, int(speed*radius))
+			else:
+				motor_left(1, self.left_forward, speed)
+				motor_right(1, self.right_forward, speed)
+		elif direction == 'backward':
+			if turn == 'right':
+				motor_left(0, self.left_forward, int(speed*radius))
+				motor_right(1, self.right_backward, speed)
+			elif turn == 'left':
+				motor_left(1, self.left_backward, speed)
+				motor_right(0, self.right_forward, int(speed*radius))
+			else:
+				motor_left(1, self.left_backward, speed)
+				motor_right(1, self.right_backward, speed)
+		elif direction == 'no':
+			if turn == 'right':
+				motor_left(1, self.left_backward, speed)
+				motor_right(1, self.right_forward, speed)
+			elif turn == 'left':
+				motor_left(1, self.left_forward, speed)
+				motor_right(1, self.right_backward, speed)
+			else:
+				motorStop()
 		else:
-			motorStop()
-	else:
-		pass
+			pass
 
+	def move_handler(self, in_q) -> None:
+		try:		
+			while True:
+				# Get some data
+				mc = in_q.get()
 
-
-
-def destroy():
-	motorStop()
-	GPIO.cleanup()             # Release resource
-
-def move_handler(in_q) -> None:
-	global rgb
-	rgb = RGB.RGB()
-	try:
-		print(setup_completed)
-		if setup_completed == False:
-			rgb.red()
-			setup()
-			rgb.blue()
-
-		while True:
-			# Get some data
-			mc = in_q.get()
-
-			if mc != None:
-				if mc.get_stop_working():
-					motorStop()
-					rgb.pink()
-					destroy()
+				if mc != None:
+					if mc.get_stop_working():
+						motorStop()
+						rgb.pink()
+						destroy()
+						in_q.task_done()
+						continue
+					
+					rgb.green()
+					move(mc.get_speed(), mc.get_direction(), mc.get_turn(), mc.get_radius())
 					in_q.task_done()
-					continue
-				
-				rgb.green()
-				move(mc.get_speed(), mc.get_direction(), mc.get_turn(), mc.get_radius())
-				in_q.task_done()
 
-			# Process the data..
-			# Indicate completion
-			#in_q.task_done()
-	except KeyboardInterrupt:
-		destroy()
+				# Process the data..
+				# Indicate completion
+				#in_q.task_done()
+		except KeyboardInterrupt:
+			destroy()
 
 if __name__ == '__main__':
 	rgb = RGB()
