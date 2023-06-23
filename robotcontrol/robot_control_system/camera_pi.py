a480352b92a7f3c33e9d2d5c24aadace920e6cb9
Bjoern Fuertges <github.bjoern@fuertges.eu>
2023-06-23 17:32:13 +0200

    rcs: camera_pi added still preview

--- robotcontrol/robot_control_system/camera_pi.py
+++ robotcontrol/robot_control_system/camera_pi.py
@@ -1,15 +1,17 @@
 import time
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 
 
 def run():
     picam = Picamera2()
 
-    picam.configure(config)
+    picam2.start_preview(Preview.NULL)
+    capture_config = picam2.create_still_configuration()
+    picam2.configure(capture_config)
 
-    picam.start()
+    picam2.start()
     time.sleep(2)
-    picam.capture_file("test-python.jpg")
+    picam2.capture_file("test-python.jpg")
 
-    picam.close()
+    picam2.close()
     print("image saved")
