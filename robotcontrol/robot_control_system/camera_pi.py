84477b202c367df9ea13429b2e65cdc499bb8a1f
Bjoern Fuertges <github.bjoern@fuertges.eu>
2023-06-23 17:32:59 +0200

    rcs: camera_pi fixed naming issue

--- robotcontrol/robot_control_system/camera_pi.py
+++ robotcontrol/robot_control_system/camera_pi.py
@@ -5,13 +5,13 @@ from picamera2 import Picamera2, Preview
 def run():
     picam = Picamera2()
 
-    picam2.start_preview(Preview.NULL)
-    capture_config = picam2.create_still_configuration()
-    picam2.configure(capture_config)
+    picam.start_preview(Preview.NULL)
+    capture_config = picam.create_still_configuration()
+    picam.configure(capture_config)
 
-    picam2.start()
+    picam.start()
     time.sleep(2)
-    picam2.capture_file("test-python.jpg")
+    picam.capture_file("test-python.jpg")
 
-    picam2.close()
+    picam.close()
     print("image saved")
