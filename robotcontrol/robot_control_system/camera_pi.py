8acbfc510070dd85d6e9cdac0e1f8fcd9627985c
Bjoern Fuertges <github.bjoern@fuertges.eu>
2023-06-23 17:54:36 +0200

    rcs: camera_pi is now a class

--- robotcontrol/robot_control_system/camera_pi.py
+++ robotcontrol/robot_control_system/camera_pi.py
@@ -1,17 +1,17 @@
-import time
 from picamera2 import Picamera2, Preview
 
+class Camera:
+    picam : Picamera2
 
-def run():
-    picam = Picamera2()
+    def __init__(self):
+        picam = Picamera2()
 
-    picam.start_preview(Preview.NULL)
-    capture_config = picam.create_still_configuration()
-    picam.configure(capture_config)
+        picam.start_preview(Preview.NULL)
+        capture_config = picam.create_still_configuration()
+        picam.configure(capture_config)
 
-    picam.start()
-    time.sleep(2)
-    picam.capture_file("test-python.jpg")
+    def take_picture(self, image_name : str):
+        picam.start()
+        picam.capture_file(image_name)
 
-    picam.close()
-    print("image saved")
+        picam.close()
