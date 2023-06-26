bacee31a52485e253c42c9dbe297554325dc6e54
Jarod Umland <jarodumland@gmail.com>
2023-06-26 14:57:29 +0200

    starting with contour detection

--- robotcontrol/machine_learning/read.py
+++ robotcontrol/machine_learning/read.py
@@ -3,13 +3,14 @@ import cv2 as cv
 #reads the image then returns it
 img = cv.imread('Photos/test.jpg')
 
-#opens the image in a new window
-cv.imshow('test', img)
-#large images can go off screen
-
 #reading videos
 
-capture = cv.VideoCapture('Videos/test.mov')
+capture = cv.VideoCapture(0)
+
+#Standard capture should be on port 0 or 1
+
+cascade = open
+
 #reading video frame by frame
 while True:
     isTrue, frame = capture.read()
@@ -20,4 +21,18 @@ while True:
 #capture.release()
 #cv.destroyAllWindows
 
-#wait till cv.waitKey(3)
+#Contour Detection
+
+#opens the image in a new window
+cv.imshow('test', img)
+#large images can go off screen
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+cv.imshow('GRAY', gray)
+
+canny = cv.canny(img, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+contours, hierarchies = cv.findContours(canny, cv.RETR_List, cv.CHAIN_APPROX_NONE)
+# contours = python list of contours / contours which object is within which
+# https://www.youtube.com/watch?v=oXlwWbU8l2o&t=777s
