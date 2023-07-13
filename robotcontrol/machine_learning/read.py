3bf2221cb283385dba83d076af74281b981b78a4
Jarod Umland <jarodumland@gmail.com>
2023-07-13 23:57:55 +0200

    greyscaling doesnt work yet

--- robotcontrol/machine_learning/read.py
+++ robotcontrol/machine_learning/read.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 #reads the image then returns it
 
 # Grayscale, Gaussian blur, Otsu's threshold
-image = cv.imread('Bild1.jpg')
+image = cv.imread('Bild1.png')
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 blur = cv.GaussianBlur(gray, (3,3), 0)
 thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
@@ -20,37 +20,41 @@ invert = 255 - opening
 # Perform text extraction
 data = pytesseract.image_to_string(invert, lang='eng', config='--psm 6')
 print(data)
+cv.imshow('thresh', thresh)
+cv.imshow('opening', opening)
+cv.imshow('invert', invert)
+cv.waitKey()
 #reading videos
 
-capture = cv.VideoCapture(0)
+#capture = cv.VideoCapture(0)
 
 #Standard capture should be on port 0 or 1
 
-cascade = open
+#cascade = open
 
 #reading video frame by frame
-while True:
-    isTrue, frame = capture.read()
-    cv.imshow('Video', frame)
+#while True:
+#    isTrue, frame = capture.read()
+#    cv.imshow('Video', frame)
 
-    if cv.waitKey(20) & 0xFF==ord('d'):
-        break
+    #if cv.waitKey(20) & 0xFF==ord('d'):
+     #   break
 #capture.release()
 #cv.destroyAllWindows
 
 #Contour Detection
 
 #opens the image in a new window
-cv.imshow('test', img)
+#cv.imshow('test', img)
 #large images can go off screen
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-cv.imshow('GRAY', gray)
+#cv.imshow('GRAY', gray)
 
-canny = cv.canny(img, 125, 175)
-cv.imshow('Canny Edges', canny)
-
-contours, hierarchies = cv.findContours(canny, cv.RETR_List, cv.CHAIN_APPROX_NONE)
+#canny = cv.canny(img, 125, 175)
+#cv.imshow('Canny Edges', canny)
+#
+#contours, hierarchies = cv.findContours(canny, cv.RETR_List, cv.CHAIN_APPROX_NONE)
 # contours = python list of contours / contours which object is within which
 # https://www.youtube.com/watch?v=oXlwWbU8l2o&t=777s
 
