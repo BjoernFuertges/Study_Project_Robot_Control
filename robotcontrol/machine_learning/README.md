22fce8f2edbcf27dc5a3771c4ebbe86b9786c1c3
Jarod Umland <jarodumland@gmail.com>
2023-07-13 23:55:09 +0200

    trying to morph picture

--- robotcontrol/machine_learning/README.md
+++ robotcontrol/machine_learning/README.md
@@ -1,9 +1,25 @@
 # Installation
 
+sudo needed
 ``` bash
 pip install opencv-contrib-python
 pip install caer
+pip install pytesseract
+pip install numpy
+pip install matplotlib
+pip install opencv-python
+pip install pillow
 ```
+
+tesseract installer needed: 
+https://github.com/UB-Mannheim/Tesseract_Dokumentation/blob/main/Tesseract_Doku_Windows.md
+
+add tesseract to path
+normal install path is: 
+"C:\Program Files\Tesseract-OCR"
+
+to run pytesseract needs to know location of the script via a code snippet at the top
+"pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'"
 
 More information on machine learning: 
 https://docs.opencv.org/3.4/dc/dd6/ml_intro.html
--- robotcontrol/machine_learning/read.py
+++ robotcontrol/machine_learning/read.py
@@ -1,8 +1,25 @@
 import cv2 as cv
+import PIL as ts
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+import matplotlib.pyplot as plt
 
 #reads the image then returns it
-img = cv.imread('Photos/test.jpg')
 
+# Grayscale, Gaussian blur, Otsu's threshold
+image = cv.imread('Bild1.jpg')
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+blur = cv.GaussianBlur(gray, (3,3), 0)
+thresh = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+
+# Morph open to remove noise and invert image
+kernel = cv.getStructuringElement(cv.MORPH_RECT, (3,3))
+opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=1)
+invert = 255 - opening
+
+# Perform text extraction
+data = pytesseract.image_to_string(invert, lang='eng', config='--psm 6')
+print(data)
 #reading videos
 
 capture = cv.VideoCapture(0)
@@ -36,3 +53,4 @@ cv.imshow('Canny Edges', canny)
 contours, hierarchies = cv.findContours(canny, cv.RETR_List, cv.CHAIN_APPROX_NONE)
 # contours = python list of contours / contours which object is within which
 # https://www.youtube.com/watch?v=oXlwWbU8l2o&t=777s
+
